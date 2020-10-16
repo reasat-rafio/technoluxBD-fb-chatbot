4ecaeeeacb924c11028f1616b7051205d912fd8c
reasat-rafio <59998970+reasat-rafio@users.noreply.github.com>
2020-10-16 07:48:12 +0600

    update

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -1,4 +1,6 @@
 const dotenv = require("dotenv");
+const request = require("request");
+
 dotenv.config({ path: "../config/config.env" });
 
 module.exports.getHomepage = (req, res) => {
@@ -42,6 +44,14 @@ module.exports.postWebHook = (req, res) => {
          // Get the sender PSID
          let sender_psid = webhook_event.sender.id;
          console.log("Sender PSID: " + sender_psid);
+
+         // Check if the event is a message or postback and
+         // pass the event to the appropriate handler function
+         if (webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);
+         } else if (webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+         }
       });
 
       // Returns a '200 OK' response to all requests
@@ -53,12 +63,94 @@ module.exports.postWebHook = (req, res) => {
 };
 
 // Handles messages events
-let handleMessage = (sender_psid, received_message) => {};
+let handleMessage = (sender_psid, received_message) => {
+   let response;
+
+   // Check if the message contains text
+   if (received_message.text) {
+      // Create the payload for a basic text message
+      response = {
+         text: `You sent the message: "${received_message.text}". Now send me an image!`,
+      };
+   } else if (received_message.attachments) {
+      // Get the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+      response = {
+         attachment: {
+            type: "template",
+            payload: {
+               template_type: "generic",
+               elements: [
+                  {
+                     title: "Is this the right picture?",
+                     subtitle: "Tap a button to answer.",
+                     image_url: attachment_url,
+                     buttons: [
+                        {
+                           type: "postback",
+                           title: "Yes!",
+                           payload: "yes",
+                        },
+                        {
+                           type: "postback",
+                           title: "No!",
+                           payload: "no",
+                        },
+                     ],
+                  },
+               ],
+            },
+         },
+      };
+   }
+
+   // Sends the response message
+   callSendAPI(sender_psid, response);
+};
 
 // Handles messaging_postbacks events
-let handlePostback = (sender_psid, received_postback) => {};
+let handlePostback = (sender_psid, received_postback) => {
+   let response;
+
+   // Get the payload for the postback
+   let payload = received_postback.payload;
+
+   // Set the response based on the postback payload
+   if (payload === "yes") {
+      response = { text: "Thanks!" };
+   } else if (payload === "no") {
+      response = { text: "Oops, try sending another image." };
+   }
+   // Send the message to acknowledge the postback
+   callSendAPI(sender_psid, response);
+};
 
 // Sends response messages via the Send API
-let callSendAPI = (sender_psid, response) => {};
+let callSendAPI = (sender_psid, response) => {
+   // Construct the message body
+   let request_body = {
+      recipient: {
+         id: sender_psid,
+      },
+      message: response,
+   };
+
+   // Send the HTTP request to the Messenger Platform
+   request(
+      {
+         uri: "https://graph.facebook.com/v6.0/me/messages",
+         qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+         method: "POST",
+         json: request_body,
+      },
+      (err, res, body) => {
+         if (!err) {
+            console.log("message sent!");
+         } else {
+            console.error("Unable to send message:" + err);
+         }
+      }
+   );
+};
 
 // curl -X GET "localhost:8080/webhook?hub.verify_token=itIsAsdasd&hub.challenge=CHALLENGE_ACCEPTED&hub.mode=subscribe"
