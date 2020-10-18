3c4f62b9968e6a471df44dd8acaf8cfcffd538e0
reasat-rafio <59998970+reasat-rafio@users.noreply.github.com>
2020-10-18 06:22:10 +0600

    update

--- services/passThreadControl.js
+++ services/passThreadControl.js
@@ -1,3 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "../config/config.env" });
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const SECONDARY_RECEIVER_ID = process.env.SECONDARY_RECEIVER_ID;
+const PRIMARY_RECEIVER_ID = process.env.FACEBOOK_APP_ID;
+
 module.exports.passThreadControl = (sender_psid, app) => {
    return new Promise(async (resolve, reject) => {
       try {
