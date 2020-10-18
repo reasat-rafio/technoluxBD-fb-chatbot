679df12627ba6b1428821b5d475f1f2e55b04d11
reasat-rafio <59998970+reasat-rafio@users.noreply.github.com>
2020-10-18 06:20:18 +0600

    update

--- services/chatbotService.js
+++ services/chatbotService.js
@@ -14,6 +14,7 @@ const {
    markMessageRead,
    sendTypingOn,
 } = require("./homePageService");
+const { passThreadControl } = require("./passThreadControl");
 dotenv.config({ path: "../config/config.env" });
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
@@ -96,51 +97,6 @@ module.exports.sendLookupOrder = (sender_psid) => {
          let response = sendLookUpTemplate();
          await sendMessage(sender_psid, response);
          resolve("done!");
-      } catch (err) {
-         reject(err);
-      }
-   });
-};
-
-module.exports.passThreadControl = (sender_psid, app) => {
-   return new Promise(async (resolve, reject) => {
-      try {
-         let target_app_id = "";
-         let metadata = "";
-
-         if (app === "page_inbox") {
-            target_app_id = SECONDARY_RECEIVER_ID;
-            metadata = "Pass thread control to inbox chat";
-         }
-         if (app === "primary") {
-            target_app_id = PRIMARY_RECEIVER_ID;
-            metadata = "Pass thread control to the bot, primary app";
-         }
-
-         let request_body = {
-            recipient: {
-               id: sender_psid,
-            },
-            target_app_id: target_app_id,
-            metadata: metadata,
-         };
-
-         // Send the HTTP request to the Messenger Platform
-         request(
-            {
-               uri: "https://graph.facebook.com/v6.0/me/pass_thread_control",
-               qs: { access_token: PAGE_ACCESS_TOKEN },
-               method: "POST",
-               json: request_body,
-            },
-            (err, res, body) => {
-               if (!err) {
-                  resolve("message sent!");
-               } else {
-                  reject("Unable to send message:" + err);
-               }
-            }
-         );
       } catch (err) {
          reject(err);
       }
