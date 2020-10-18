ee1150bf7aa07af38be642c3c00de0324766379e
reasat-rafio <59998970+reasat-rafio@users.noreply.github.com>
2020-10-18 06:11:55 +0600

    update

--- services/backToCategories.js
+++ services/backToCategories.js
@@ -2,12 +2,16 @@ const { sendCategories } = require("./chatbotService");
 const dotenv = require("dotenv");
 dotenv.config({ path: "../config/config.env" });
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const SECONDARY_RECEIVER_ID = process.env.SECONDARY_RECEIVER_ID;
+const PRIMARY_RECEIVER_ID = process.env.FACEBOOK_APP_ID;
+
 module.exports.backToCategories = (sender_psid) => {
    sendCategories(sender_psid);
 };
 
 module.exports.passThreadControl = (sender_psid, app) => {
-   new Promise(async (resolve, reject) => {
+   return new Promise(async (resolve, reject) => {
       try {
          let target_app_id = "";
          let metadata = "";
